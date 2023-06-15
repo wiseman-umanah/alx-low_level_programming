e5c141f92b4b34c321d00332ea10c80b67e152e5
wiseman-umanah <wisemanumanah@gmail.com>
2023-06-15 12:57:58 -0700

    My first C program in alx

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-	char a[100] = "Programming is like building a multilingual puzzle";
+	char a[100] = "\"Programming is like building a multilingual puzzle";
 
 	puts(a);
 	return (0);
