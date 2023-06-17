d9a5a08d2c3162aca384aff7f6e89ec7f456d660
wiseman-umanah <wisemanumanah@gmail.com>
2023-06-17 12:41:30 -0700

    Added a new C file

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: 0
+*/
+int main(void)
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+
+	while (a <= 7)
+	{
+		b = a + 1;
+		while (b <= 8)
+		{
+			c = b + 1;
+			while (c <= 9)
+			{
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(c + '0');
+				if (a != 7 || b != 8 || c != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				c++;
+			}
+			b++;
+		}
+		a++;
+		putchar('\n');
+	}
+	return (0);
+}
