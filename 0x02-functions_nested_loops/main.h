14d9a75b1a261c8b7e609cb9273efe7bbbf7f61b
wiseman-umanah <126745459+wiseman-umanah@users.noreply.github.com>
2023-06-20 18:48:22 +0100

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,14 +1,24 @@
-#include <stdio.h>
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
-* main - Entry point
-*
-* Return: always 0
-*/
+/*
+ * File: main.h
+ * Auth: Wiseman Umanah
+ * Desc: Header file containing declarations for all functions
+ *       used in the 0x02-functions_nested_loops directory.
+ */
 
 void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
 
-int main(void)
-{
-	return (0);
-}
+#endif
