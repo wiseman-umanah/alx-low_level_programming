ee73bbd7b034a97982ce2a574189576991d5a07d
wiseman-umanah <wisemanumanah@gmail.com>
2023-06-21 00:48:50 -0700

    Created a new c file

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* main - Entry point
+*
+* Return: 0
+*/
+int main(void)
+{
+	int num;
+	int i;
+
+	num = 1
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			num += i;
+		}
+	}
+	printf("%d\n", num);
+	return (0);
+}
