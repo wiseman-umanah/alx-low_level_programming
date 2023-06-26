223d00ed28f288249f39d8b3397643760a0a0f7c
wiseman-umanah <126745459+wiseman-umanah@users.noreply.github.com>
2023-06-26 17:04:27 +0100

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -19,7 +19,7 @@ void puts_half(char *str)
 	}
 	else
 	{
-		for (i = (strlen(str) - 1) / 2; i < strlen(str); i++)
+		for (i = ((strlen(str) - 1) / 2) + 1; i < strlen(str); i++)
 		{
 			_putchar(str[i]);
 		}
