cf4139fc329ad1f0cf33ebb7764c03bd060b7b1c
wiseman-umanah <wisemanumanah@gmail.com>
2023-06-26 07:13:22 -0700

    Added a new file

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+* print_rev - prints string in reversed order
+*
+* @s: paramater
+*/
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = (strlen(s) - 1); i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+}
