23013fbedeab2ece29f5756dbee61b94ecac8483
wiseman-umanah <wisemanumanah@gmail.com>
2023-06-26 15:31:59 -0700

    Added a new file

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Entry point
+*
+* Return: Always (0)
+*/
+int main(void)
+{
+	int i;
+
+	srand(time(NULL));
+
+	for (i = 0; i <= 10; i++)
+	{
+		prinf("%d\n", rand() % 28379 + 1);
+	}
+	return (0);
+}
