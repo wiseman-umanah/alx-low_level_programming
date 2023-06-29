7058f4a5b5f38d1b3c62f0ac5996e3e14e381286
wiseman-umanah <126745459+wiseman-umanah@users.noreply.github.com>
2023-06-29 22:31:28 +0100

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,36 +1,21 @@
 #include "main.h"
-
 /**
-* reverse_array - reverses an array
-*
-* @a: The array
-* @n: paramter
-*/
-
+ *reverse_array - revereses an array.
+ *@a: pointer to array.
+ *@n: number of elements of an array.
+ *
+ *Return: void.
+ */
 void reverse_array(int *a, int n)
 {
-	int i;
-	int j;
-	int b[900];
+	int temp, counter;
 
-	j = 0;
-	for (i = (n - 1); i >= 0; i--)
+	n = n - 1;
+	counter = 0;
+	while (counter <= n)
 	{
-		b[j] = a[i];
-		j++;
-	}
-	for (i = 0; i < n; i++)
-	{
-		a[i] = b[i];
-		if (i == (n - 1))
-		{
-			_putchar(a[i]);
-		}
-		else
-		{
-			_putchar(a[i]);
-			_putchar(',');
-			_putchar(' ');
-		}
+		temp = a[counter];
+		a[counter++] = a[n];
+		a[n--] = temp;
 	}
 }
