9946be2a0710fdf55e357c21c63cc319672352e3
wiseman-umanah <wisemanumanah@gmail.com>
2023-06-29 20:37:18 +0100

    Added a new C file

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* *string_toupper - changes all lowercase string to uppercase
+*
+* @a: parameter
+*
+* Return: returns uppercase
+*/
+
+char *string_toupper(char *a)
+{
+	return (strupr(a));
+}
