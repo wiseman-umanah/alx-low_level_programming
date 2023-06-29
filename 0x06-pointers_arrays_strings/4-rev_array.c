f2184ea12455c632bdf9d5bad1d63d480aa33db9
wiseman-umanah <wisemanumanah@gmail.com>
2023-06-29 21:06:19 +0100

    Fixed bug

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -14,18 +14,18 @@ void reverse_array(int *a, int n)
 	int b[900];
 
 	j = 0;
-	for (i = (n-1); i >= 0; i--)
+	for (i = (n - 1); i >= 0; i--)
 	{
 		b[j] = a[i];
 		j++;
 	}
 	for (i = 0; i < n; i++)
-        {
+	{
 		a[i] = b[i];
-		if (i == (n-1))
+		if (i == (n - 1))
 		{
 			_putchar(a[i]);
-		}							
+		}
 		else
 		{
 			_putchar(a[i]);
