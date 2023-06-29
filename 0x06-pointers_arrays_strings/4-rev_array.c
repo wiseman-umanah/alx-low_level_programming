dc2e7be473542183bca8f6225fa0e970b42d5801
wiseman-umanah <wisemanumanah@gmail.com>
2023-06-29 16:39:21 +0100

    Removed wrong file and added new C file.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+* reverse_array - reverses an array
+*
+* @a: The array
+* @n: paramter
+*/
+
+void reverse_array(int *a, int n)
+{
+	int i;
+	int j;
+	int b[900];
+
+	j = 0;
+	for (i = (n-1); i >= 0; i--)
+	{
+		b[j] = a[i];
+		j++;
+	}
+	for (i = 0; i < n; i++)
+        {
+		a[i] = b[i];
+		if (i == (n-1))
+		{
+			_putchar(a[i]);
+		}							
+		else
+		{
+			_putchar(a[i]);
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+}
