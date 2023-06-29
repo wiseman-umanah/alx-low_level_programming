5f1cc99f2015aa66f0944041ecebd4bf949c4861
wiseman-umanah <wisemanumanah@gmail.com>
2023-06-29 06:32:12 -0700

    Added a new C file

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* _strcmp - compares two strings
+*
+* @s1: first parameter
+* @s2: second paramteter
+*
+* Return: returns the compared string
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
