1de3990303f9d19247667ba266315cd111b69655
wiseman-umanah <wisemanumanah@gmail.com>
2023-06-29 22:25:56 +0100

    Fixed bug

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,14 +1,27 @@
-char *rot13(char *str)
+#include "main.h"
+
+/**
+*rot13 - encodes strings using rot13.
+*@s: pointer to string.
+*
+*Return: pointer to encoded string.
+*/
+
+char *rot13(char *s)
 {
-    char *p = str;
+	char *p = s;
 
-    while (*p) {
-        if ((*p >= 'a' && *p <= 'm') || (*p >= 'A' && *p <= 'M')) {
-            *p += 13;
-        } else if ((*p >= 'n' && *p <= 'z') || (*p >= 'N' && *p <= 'Z')) {
-            *p -= 1}
-	p++;
-    }
-
-    return str;
+	while (*p)
+	{
+		if ((*p >= 'a' && *p <= 'm') || (*p >= 'A' && *p <= 'M'))
+		{
+			*p += 13;
+		}
+		else if ((*p >= 'n' && *p <= 'z') || (*p >= 'N' && *p <= 'Z'))
+		{
+			*p -= 1
+		}
+		p++;
+	}
+	return (s);
 }
