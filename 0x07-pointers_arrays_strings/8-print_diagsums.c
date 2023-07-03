31ef5e65521f8dfbeff1314dcfae59d5c5b0bbbf
wiseman-umanah <126745459+wiseman-umanah@users.noreply.github.com>
2023-07-03 22:24:19 +0100

    Update 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,9 +11,11 @@
 void print_diagsums(int *a, int size)
 {
 	int i;
-	unsigned int sum1, sum2;
+	unsigned int sum1;
+	unsigned int sum2;
 
-	sum1 = sum2 = 0;
+	sum1 = 0;
+	sum2 = 0;
 	for (i = 0; i < (size * size); i += (size + 1))
 	{
 		sum1 += a[i];
