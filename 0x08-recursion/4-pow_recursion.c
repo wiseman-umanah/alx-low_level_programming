bcfa7fabcb756e789ca0b368924961032d4431be
wiseman-umanah <wisemanumanah@gmail.com>
2023-07-05 15:38:35 +0100

    Fixed bug

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -12,7 +12,9 @@ int _pow_recursion(int x, int y)
 	int pow;
 
 	pow = 0;
-	if (y < 0)
+	if (y == 0)
+		return (1);
+	else if (y < 0)
 		return (-1);
 	pow = x * _pow_recursion(x, y - 1);
 	return (pow);
