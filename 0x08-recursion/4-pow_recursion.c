b5a66c8ee396eefe3eca8cfcf0524a71efa353b3
wiseman-umanah <wisemanumanah@gmail.com>
2023-07-05 15:29:45 +0100

    Added a new C file

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _pow_recursion - finds x to pow of y
+* @x: base number
+* @y: power
+* Return: x to pow of y
+*/
+
+int _pow_recursion(int x, int y)
+{
+	int pow;
+
+	pow = 0;
+	if (y < 0)
+		return (-1);
+	pow = x * _pow_recursion(x, y - 1);
+	return (pow);
+}
