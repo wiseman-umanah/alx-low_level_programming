755fbffef130b019c916caf16f470eb2f15fba7e
wiseman-umanah <wisemanumanah@gmail.com>
2023-07-06 07:13:48 +0100

    Checking something in code

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
-#include <string.h>
 
 /**
 * _puts_recursion - prints string with new line
 *
 * @s: string to print
+* Return: returns the string with a new line
 */
 
 void _puts_recursion(char *s)
 {
-	puts(s);
+	if (*s == '\0')
+	{
+		putchar('\n');
+		return;
+	}
+	putchar(*s);
+	_puts_recursion(s + 1);
 }
