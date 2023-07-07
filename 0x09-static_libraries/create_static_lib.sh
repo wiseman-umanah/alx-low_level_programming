f023eddfa30c8fdf9c08059b0f59467f0814a80f
wiseman-umanah <126745459+wiseman-umanah@users.noreply.github.com>
2023-07-07 17:40:40 +0100

    Update create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-find . -type f -name '*.c' -exec gcc -Wall -pedantic -Werror -Wextra -c {} \;
-find . -type f -name '*.o' -exec ar rcs liball.a {} \;
+gcc -Wall -pedantic -Werror -Wextra -c *.c
+ar rc liball.a *.o
