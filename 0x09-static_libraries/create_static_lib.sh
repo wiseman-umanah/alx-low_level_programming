36b50188c8123b201c9e3213ba0672622c47f403
wiseman-umanah <wisemanumanah@gmail.com>
2023-07-07 17:39:01 +0100

    this should work

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-find . -type f -name '*.c' -exec gcc -c {} \;
+find . -type f -name '*.c' -exec gcc -Wall -pedantic -Werror -Wextra -c {} \;
 find . -type f -name '*.o' -exec ar rcs liball.a {} \;
