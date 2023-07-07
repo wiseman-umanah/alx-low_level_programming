acd2ba294cdb1efe826968987614de33e1638cd3
wiseman-umanah <126745459+wiseman-umanah@users.noreply.github.com>
2023-07-08 00:33:21 +0100

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
-
 /**
-* main - Entry point
-*
-* @argc: argument count
-*
-* Return: always 0
-*/
-
+  *main - prints the number of arguments passed to it.
+  *@argc: number of arguemnts.
+  *@argv: array of size argc.
+  *
+  *Return: 0 on success.
+  */
 int main(int argc, char *argv[])
 {
-	if (argc == 0)
-		printf("
-	printf("%d\n", argc - 1);
+	int i;
+	(void)argv;
+
+	for (i = 0; i < argc - 1; i++)
+	{}
+
+	printf("%d\n", i);
 	return (0);
 }
