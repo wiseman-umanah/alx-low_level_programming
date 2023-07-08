d4c94f436977f232084458c29dc1163c95804f6e
wiseman-umanah <126745459+wiseman-umanah@users.noreply.github.com>
2023-07-08 21:54:59 +0100

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,46 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-
 /**
-* main - Entry point
-*
-* @argc: argument count
-* @argv: arguments
-*
-* Return: 0 if no error, 1 if found
-*/
-
+  *main - adds and prints sum of positive numbers
+  *followed by a new line.
+  *@argc: number of arguments passed.
+  *@argv: pointer to array of size argc, containing
+  *arguments passed.
+  *
+  *Return: 0 on success.
+  *1 on error.
+  */
 int main(int argc, char *argv[])
 {
-	int i;
-	int j;
-	int sum;
+	int count, count2, sum;
 
 	sum = 0;
-	if (argc == 0)
-	{
-		printf("%d\n", sum);
-	}
-	else
-	{
-		for (i = 1; i < argc; i++)
-		{
-			for (j = 0; argv[i][j] != '\0'; j++)
-			{
-				if (isdigit(argv[i][j]))
-				{
-					sum += atoi(argv[i]);
 
-				}
-				else
-				{
-					puts("Error");
-					return (1);
-				}
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (count = 1; count < argc; count++)
+	{
+		for (count2 = 0; argv[count][count2] != '\0'; count2++)
+		{
+			if (argv[count][count2] < '0' || argv[count][count2] > '9')
+			{
+				printf("Error\n");
+				return (1);
 			}
 		}
-		printf("%d\n", sum);
+		sum += atoi(argv[count]);
 	}
+	printf("%d\n", sum);
 	return (0);
 }
