47c861a98702bbb9a63ff6b76fde74c399a4e959
wiseman-umanah <wisemanumanah@gmail.com>
2023-07-11 18:22:30 +0100

    Freed memory

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -27,6 +27,10 @@ int **alloc_grid(int width, int height)
 		num = (int **)malloc(height * sizeof(int *));
 		if (num == NULL)
 		{
+			for (i = 0; i < height; i++)
+			{
+				free(num[i]);
+			}
 			free(num);
 			return (num);
 		}
