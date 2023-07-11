e6b05b44254c50ad128bd63fb5edfc58316aa6e7
wiseman-umanah <wisemanumanah@gmail.com>
2023-07-11 21:05:25 +0100

    Fixed code

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,9 +18,9 @@ char *str_concat(char *s1, char *s2)
 	int len2;
 	int tlen;
 
-	if (*s1 == '\0')
+	if (s1 == NULL)
 		s1 = "";
-	if (*s2 == '\0')
+	if (s2 == NULL)
 		s2 = "";
 	len2 = strlen(s2);
 	len1 = strlen(s1);
