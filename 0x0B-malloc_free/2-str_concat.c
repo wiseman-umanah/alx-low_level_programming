c4d4b7eeeaf450c1779d51fcb5a1e94abe56c7fd
wiseman-umanah <wisemanumanah@gmail.com>
2023-07-11 20:49:27 +0100

    Added a new C file

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* str_concat - string concatenation with malloc
+*
+* @s1: first string
+* @s2: second string
+*
+* Return: returns pointer to string or NULL if false
+*/
+
+char *str_concat(char *s1, char *s2)
+{
+	char *ptr;
+	int len1;
+	int len2;
+	int tlen;
+
+	if (*s1 == '\0')
+		s1 = "";
+	if (*s2 == '\0')
+		s2 = "";
+	len2 = strlen(s2);
+	len1 = strlen(s1);
+	tlen = len1 + len2 + 1;
+	ptr = (char *)malloc(tlen);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	strcpy(ptr, s1);
+	strcat(ptr, s2);
+	ptr[tlen] = '\0';
+	return (ptr);
+}
