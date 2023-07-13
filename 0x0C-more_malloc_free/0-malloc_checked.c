cafa6d2ac5af90d8acee347b3276a404c659a6c5
wiseman-umanah <wisemanumanah@gmail.com>
2023-07-13 12:39:35 +0100

    We begin another project

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* malloc_checked - returns pointer to allocated memory
+*
+* @b: size of malloc
+*
+* Return: 98 if failed, address to allocated memory
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+	{
+		return ((int *)98);
+	}
+	else
+		return (ptr);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,2 @@
+WIll update this later 
+
