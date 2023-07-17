4926ad62485b9acc5e51411c3d334d9b94ba0f7b
wiseman-umanah <wisemanumanah@gmail.com>
2023-07-17 16:48:06 +0100

    Fixed error in header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,16 +16,7 @@ struct dog
 	char *owner;
 };
 
-/**
-* struct dog_t - second dog detail
-*/
-
-typedef struct
-{
-	char *name;
-	float age;
-	char *owner;
-} dog_t;
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
