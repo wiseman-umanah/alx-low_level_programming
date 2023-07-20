e1789b27cc7dfda32012b13723ba326e0fedda6c
wiseman-umanah <wisemanumanah@gmail.com>
2023-07-20 16:09:46 +0100

    Started a new project -variadic functions

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+* sum_them_all - finds the sum of n numbers
+*
+* @n: number of args
+* Return: return sum if success, 0 otherwise
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i, result, x;
+
+	if (n <= 0)
+		return (0);
+	va_list args;
+
+	va_start(args, n);
+	result = 0;
+	for (i = 0; i < n; i++)
+	{
+		x = va_arg(args, unsigned int);
+		result += x;
+	}
+	va_end(args);
+	return (result);
+}
