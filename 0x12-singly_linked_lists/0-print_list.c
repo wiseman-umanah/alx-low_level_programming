630b1d6ae5d2496891e8d0bc8c7ee905f78b1e0d
wiseman-umanah <126745459+wiseman-umanah@users.noreply.github.com>
2023-07-28 02:03:18 +0100

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,12 +14,15 @@ size_t print_list(const list_t *h)
 	size_t i = 0;
 
 	temp = h;
-	while (temp != 0)
+	while (temp != NULL)
 	{
 		if (temp->str == NULL)
+		{
 			printf("[%d] %s\n", 0, "(nil)");
-		else
-			printf("[%d] %s\n", temp->len, temp->str);
+			temp = temp->next;
+			i++;
+		}
+		printf("[%d] %s\n", temp->len, temp->str);
 		i++;
 		temp = temp->next;
 	}
