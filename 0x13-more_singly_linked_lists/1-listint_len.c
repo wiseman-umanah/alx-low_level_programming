28959c276acdfbe74ee1b59f9add616664a1d48d
wiseman-umanah <wisemanumanah@gmail.com>
2023-08-01 19:13:05 +0100

    Fixed compile error

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -7,14 +7,13 @@
 * Return: the number of nodes.
 */
 
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	int count;
 
 	count = 0;
 	while (h != NULL)
 	{
-		printf("%d\n", h->n);
 		h = h->next;
 		count++;
 	}
