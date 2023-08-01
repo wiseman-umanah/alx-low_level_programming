842f4405265efc9465ee2a8326981cc17031aac1
wiseman-umanah <wisemanumanah@gmail.com>
2023-08-01 19:15:02 +0100

    Fixed betty error

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
-* print_listint - prints all the number of elements of a list_t.
+* listint_len - prints all the number of elements of a list_t.
 * @h: pointer to list.
 *
 * Return: the number of nodes.
