4cbacfc94e21c92d200c122c4bfeb3f89995760e
Wiseman Umanah <126745459+wiseman-umanah@users.noreply.github.com>
2023-08-08 01:03:04 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,14 +14,17 @@ int create_file(const char *filename, char *text_content)
 	i = 0;
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_RDONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd < 0)
 		return (-1);
-	while (text_content[i] != '\0')
-		i++;
-	wr = write(fd, text_content, i);
-	if (wr < 0)
-		return (-1);
+	if (text_content)
+	{
+		while (text_content[i] != '\0')
+			i++;
+		wr = write(fd, text_content, i);
+		if (wr < 0)
+			return (-1);
+	}
 	close(fd);
 	return (1);
 }
