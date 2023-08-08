5c90497ae58f3a700e1c9482b4505f14f2b7c81d
Wiseman Umanah <126745459+wiseman-umanah@users.noreply.github.com>
2023-08-08 01:07:12 +0100

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,14 +13,18 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_APPEND | O_WRONLY);
+	fd = open(filename, O_EXCL | O_APPEND | O_WRONLY);
 	if (fd < 0)
 		return (-1);
-	while (text_content[i] != '\0')
-		i++;
-	wr = write(fd, text_content, i);
-	if (wr < 0)
-		return (-1);
+	if (text_content)
+	{
+		i = 0;
+		while (text_content[i] != '\0')
+			i++;
+		wr = write(fd, text_content, i);
+		if (wr < 0)
+			return (-1);
+	}
 	close(fd);
 	return (1);
 }
