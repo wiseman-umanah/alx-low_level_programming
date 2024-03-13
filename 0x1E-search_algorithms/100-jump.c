38758e7d0a6b699cfdec6a6810113f66843efe0c
wiseman-umanah <wisemanumanah@gmail.com>
2024-03-13 16:00:15 +0100

    Restructed code

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,6 +1,15 @@
 #include <math.h>
 #include "search_algos.h"
 
+/**
+ * pretty_print - Prints each checked value
+ * @array: the array
+ * @start: the start value
+ * @value: the value to search
+ * @end: the end of array
+ * Return: -1 for not seen, index of value
+*/
+
 int pretty_print(int *array, int start, int end, int value)
 {
 	int j;
@@ -14,28 +23,37 @@ int pretty_print(int *array, int start, int end, int value)
 	return (-1);
 }
 
+
+/**
+ * jump_search - The jump algorithm
+ * @array: the array
+ * @size: the size of the array
+ * @value: The value to find
+ * Return: -1 for not seen, index of value
+*/
+
 int jump_search(int *array, size_t size, int value)
 {
-    int step;
-    size_t start, i;
+	int step;
+	size_t start, i;
 
-    start = i = 0;
-    step = sqrt(size);
-    for (; i < size;)
-    {
-        printf("Value checked array[%d] = [%d]\n", (int)i, array[i]);
-        if (array[i] == value)
+	start = i = 0;
+	step = sqrt(size);
+	for (; i < size;)
+	{
+		printf("Value checked array[%d] = [%d]\n", (int)i, array[i]);
+		if (array[i] == value)
 		{
 			printf("Value found between indexes [%d] and [%d]\n", (int)start, (int)i);
 			return (pretty_print(array, start, ++i, value));
 		}
-        if (array[i] > value)
+		if (array[i] > value)
 		{
 			printf("Value found between indexes [%d] and [%d]\n", (int)start, (int)i);
-            return (pretty_print(array, start, i, value));
+			return (pretty_print(array, start, i, value));
 		}
 		start = i;
 		i += step;
-    }
-    return (-1);
+	}
+	return (-1);
 }
